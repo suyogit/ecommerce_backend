9e4d90d3e4f2aae2bdc0e71473777113b1c1339c
Suyog Acharya <asuyog042@gmail.com>
2023-07-04 06:05:59 +0545

    basic environment and getallproducts route created

---
+++ backend/routes/productRoute.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const { getAllProducts } = require("../controller/productController");
+
+const router = express.Router();
+
+router.route("/products").get(getAllProducts);
+
+
+
+
+module.exports = router
