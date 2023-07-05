3097aba9adc4468c62e4430d154df08a889d7d2f
Suyog Acharya <asuyog042@gmail.com>
2023-07-05 21:10:44 +0545

    connecting mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const app = require("./app")
 const dotenv = require("dotenv")
-
+const connectDatabase = require("./config/database")
 
 dotenv.config({ path: "./backend/config/config.env" })
+
+
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
