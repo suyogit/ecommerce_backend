f248869781947de5491e53d082479e73f350f3c6
Suyog Acharya <asuyog042@gmail.com>
2023-07-06 21:17:32 +0545

    mongodb connected succesfully

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const connectDatabase = require("./config/database")
 
 dotenv.config({ path: "./backend/config/config.env" })
 
+connectDatabase()
 
 
 app.listen(process.env.PORT, () => {
@@ -11,3 +12,4 @@ app.listen(process.env.PORT, () => {
 
 }
 )
+//npm run dev to start the server
