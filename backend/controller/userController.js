84ddf811657ced51cea79d8961078f712819f413
Suyog Acharya <asuyog042@gmail.com>
2023-07-08 15:47:53 +0545

    reset password feature working

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -3,6 +3,7 @@ const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const User = require('../models/userModel')
 const sendToken = require('../utils/jwtToken');
 const sendEmail = require('../utils/sendEmail');
+const crypto = require('crypto');
 
 //Register a user=>/api/v1/register
 
@@ -92,3 +93,25 @@ exports.forgotPassword = catchAsyncErrors(async (req, res, next) => {
 }
 )
 
+//reset password=>/api/v1/password/reset/:token
+exports.resetPassword = catchAsyncErrors(async (req, res, next) => {
+    //creating hash url token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex');
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+    if (!user) {
+        return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler('Password does not match', 400))
+    }
+    //setup new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save()
+    sendToken(user, 200, res)
+}
+)
