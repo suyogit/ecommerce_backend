ccba6913f3ba920c6ff07c80b04773a3446b3c62
Suyog Acharya <asuyog042@gmail.com>
2023-07-09 13:07:56 +0545

    get single order by admin and get logged in user orders

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -34,3 +34,27 @@ exports.newOrder = catchAsyncErrors(async (req, res, next) => {
 }
 )
 
+// get  single order => /api/v1/order/:id   //:id is the id of the order that we want to get from the database    //this route is only accessible by the admin
+exports.getSingleOrder = catchAsyncErrors(async (req, res, next) => {
+    const order = await Order.findById(req.params.id).populate('user', 'name email')//populate is used to get the user details from the user collection. the second parameter is used to specify the fields that we want to get from the user collection
+    if (!order) {
+        return next(new ErrorHandler('No Order found with this ID', 404))
+    }
+    res.status(200).json({
+        success: true,
+        order
+    })
+}
+)
+
+// get logged in user orders => /api/v1/orders/me
+
+exports.myOrders = catchAsyncErrors(async (req, res, next) => {
+    const orders = await Order.find({ user: req.user._id })
+    res.status(200).json({
+        success: true,
+        orders
+    })
+}
+)
+
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const router = express.Router();
-const { newOrder } = require("../controller/orderController");
+const { newOrder, getSingleOrder, myOrders } = require("../controller/orderController");
 
 
 router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/:id").get(isAuthenticatedUser, authorizeRoles("admin"), getSingleOrder);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
 
 
 module.exports = router
