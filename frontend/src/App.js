4c48732aa9e3b41ec6409c353071f5a14d8eace2
Suyog Acharya <asuyog042@gmail.com>
2023-07-09 20:33:23 +0545

    Header (Navbar) added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Header from './component/layout/Header/Header';
+import { BrowserRouter as Router } from 'react-router-dom';
+import WebFont from 'webfontloader';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  React.useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Roboto', 'Droid Sans', 'Chilanka']
+      }
+    });
+  }, []);
+
+
+
+  return ( 
+    <Router>
+      <Header />
+    </Router>);
 }
+
 
 export default App;
